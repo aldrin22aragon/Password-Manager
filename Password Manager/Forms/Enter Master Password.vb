b552f434dd493378400bde940dcaa640fc052138
aldrin22aragon@gmail.com <aldrin22aragon@gmail.com>
2024-03-08 05:10:45 +0800

    2024-03-08

--- Password Manager/Forms/Enter Master Password.vb
+++ Password Manager/Forms/Enter Master Password.vb
@@ -1,10 +1,18 @@
 ﻿Public Class Enter_Master_Password
-   Private Sub TextBox1_KeyUp(sender As Object, e As KeyEventArgs) Handles TextBox1.KeyUp
-      Select Case e.KeyCode
-         Case Keys.Enter
+    Enum EnumResponse As Integer
+        NONE = 0
+        Enter = 1
+        Escape = 2
+    End Enum
+    Public Response As EnumResponse = Enter_Master_Password.EnumResponse.NONE
+    Private Sub TextBox1_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBox1.KeyPress
+        Dim aa As String = e.KeyChar
+        If aa = vbCr Then
+            Response = EnumResponse.Enter
             DialogResult = DialogResult.OK
-         Case Keys.Escape
-            DialogResult = DialogResult.Cancel
-      End Select
-   End Sub
+        ElseIf aa = ChrW(27) Then
+            Response = EnumResponse.Escape
+            DialogResult = DialogResult.OK
+        End If
+    End Sub
 End Class
--- Password Manager/Forms/Form1.vb
+++ Password Manager/Forms/Form1.vb
@@ -1,69 +1,86 @@
 ﻿Public Class FormMain
-   Friend WithEvents MasterPasswordExpire As New AOA_Timer(60)
-   Friend WithEvents ClipboardExpire As New AOA_Timer(30)
-   Private Sub Main_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-      IO.Directory.CreateDirectory(s)
-   End Sub
-   Dim meVisible As Boolean = False
-   Property FormVisible As Boolean
-      Get
-         Return meVisible
-      End Get
-      Set(value As Boolean)
-         meVisible = value
-         ShowInTaskbar = meVisible
-         If value Then
-            Me.Show()
-         Else
-            Me.Hide()
-         End If
-         If meVisible Then
-            Dim pass As Passwords = fscPasswords.GetSettings()
-            If pass Is Nothing Then
-               Dim g As New Generate_New_Password
-               If g.ShowDialog() = DialogResult.OK Then
-                  Dim aa As New Passwords() With {.masterPassword = g.TextBox1.Text}
-                  fscPasswords.SetSettings(aa)
-               Else
-                  FormVisible = False
-               End If
+    Friend WithEvents MasterPasswordExpire As New AOA_Timer(10)
+    Friend WithEvents ClipboardExpire As New AOA_Timer(30)
+    Private Sub Main_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        IO.Directory.CreateDirectory(s)
+    End Sub
+    Dim meVisible As Boolean = False
+    Property FormVisible As Boolean
+        Get
+            Return meVisible
+        End Get
+        Set(value As Boolean)
+            meVisible = value
+            ShowInTaskbar = value
+            If value Then
+                Me.Show()
             Else
-Enter_Master_Password:
-               Dim m As New Enter_Master_Password
-               If m.ShowDialog = DialogResult.OK Then
-                  Dim p As String = m.TextBox1.Text
-                  If pass.masterPassword = p Then
-                     'do nothing kase valid ang password
-                  Else
-                     ErrMsg("Wrong password")
-                     GoTo Enter_Master_Password
-                  End If
-               Else
-                     FormVisible = False
-               End If
+                Me.Hide()
             End If
-         End If
-      End Set
-   End Property
-   Dim forceClose As Boolean = False
-   Private Sub Main_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
-      If Not forceClose Then
-         e.Cancel = True
-         FormVisible = False
-         NotifyIcon1.ShowBalloonTip(50000)
-      End If
+        End Set
+    End Property
+    Dim forceClose As Boolean = False
+    Private Sub Main_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
+        If Not forceClose Then
+            e.Cancel = True
+            FormVisible = False
+            NotifyIcon1.ShowBalloonTip(50000)
+        End If
 
-   End Sub
+    End Sub
 
-   Private Sub NotifyIcon1_MouseClick(sender As Object, e As MouseEventArgs) Handles NotifyIcon1.MouseClick
-      If FormVisible Then
-         FormVisible = False
-      Else
-         FormVisible = True
-      End If
-   End Sub
+    Private Sub NotifyIcon1_MouseClick(sender As Object, e As MouseEventArgs) Handles NotifyIcon1.MouseClick
+        If FormVisible Then
+            FormVisible = False
+        Else
+            FormVisible = True
+            AskMasterPassword()
+        End If
+    End Sub
 
-   Private Sub FormMain_Shown(sender As Object, e As EventArgs) Handles Me.Shown
-      FormVisible = False
-   End Sub
+    Private Sub FormMain_Shown(sender As Object, e As EventArgs) Handles Me.Shown
+        FormVisible = False
+    End Sub
+
+    Private Sub MasterPasswordExpire_Tick(e As AOA_Timer.TickEventInfo) Handles MasterPasswordExpire.Tick
+        If e.IsTimeReached Then
+            ToolStripLabel1.Text = "Master Password: Expired"
+            FormVisible = True
+            AskMasterPassword()
+        Else
+            ToolStripLabel1.Text = "Master Password: Expires in " & e.Span.TotalSeconds
+        End If
+    End Sub
+    Sub AskMasterPassword()
+        Dim pass As Passwords = fscPasswords.GetSettings()
+        If pass Is Nothing Then
+            Dim g As New Generate_New_Password
+            If g.ShowDialog() = DialogResult.OK Then
+                Dim aa As New Passwords() With {.masterPassword = g.TextBox1.Text}
+                fscPasswords.SetSettings(aa)
+            Else
+                FormVisible = False
+            End If
+        Else
+            Dim m As New Enter_Master_Password
+Show:
+            Dim dRes As DialogResult = m.ShowDialog()
+            If dRes = DialogResult.OK Then
+                Select Case m.Response
+                    Case Enter_Master_Password.EnumResponse.Enter
+                        If m.TextBox1.Text = pass.masterPassword Then
+                            'do nothing entered passworod is valid
+                            MasterPasswordExpire.RestartTimer()
+                        Else
+                            ErrMsg("Invalid password. Please try again.")
+                            GoTo Show ' Ask again for the password
+                        End If
+                    Case Enter_Master_Password.EnumResponse.Escape
+                        FormVisible = False
+                End Select
+            Else
+                FormVisible = False
+            End If
+        End If
+    End Sub
 End Class
